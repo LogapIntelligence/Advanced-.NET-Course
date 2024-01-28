2d98b795682c1ad615bbb1934ee3b2e851e16f5d
LogapIntelligence <145349749+LogapIntelligence@users.noreply.github.com>
2024-01-28 12:02:36 +0200

    Initialize Course

---
+++ Advanced .NET Course (8.0)/Advanced .NET Course (8.0)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advanced_.NET_Course__8._0_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
